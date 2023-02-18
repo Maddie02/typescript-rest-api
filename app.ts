e52b47d2884a061f50bafa4d020666bd098a5fde
Maddie02 <madlensarkisian@gmail.com>
2023-02-18 23:06:29 +0200

    Configure express app

---
+++ app.ts
@@ -0,0 +1,47 @@
+import express from 'express'
+import * as http from 'http'
+
+import * as winston from 'winston'
+import * as expressWinston from 'express-winston'
+import cors from 'cors'
+import { CommonRoutesConfig } from './common/common.routes.config'
+import { UserRoutes } from './users/users.routes.config'
+import debug from 'debug'
+
+const app: express.Application = express()
+const server: http.Server = http.createServer(app)
+const port = 3000
+const routes: Array<CommonRoutesConfig> = []
+const debugLog: debug.IDebugger = debug('app')
+
+app.use(express.json())
+app.use(cors())
+
+const loggerOptions: expressWinston.LoggerOptions = {
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+        winston.format.json(),
+        winston.format.prettyPrint(),
+        winston.format.colorize({ all: true })
+    ),
+}
+
+if (!process.env.DEBUG) {
+    loggerOptions.meta = false
+}
+
+app.use(expressWinston.logger(loggerOptions))
+
+routes.push(new UserRoutes(app))
+
+const runningMessage = `Server running at http://localhost:${port}`
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.status(200).send(runningMessage)
+})
+
+server.listen(port, () => {
+    routes.forEach((route: CommonRoutesConfig) => {
+        debugLog(`Routes configured for ${route.getName()}`)
+    })
+    console.log(runningMessage)
+})
