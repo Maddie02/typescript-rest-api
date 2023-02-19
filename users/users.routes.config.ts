52528d4bd79018e12f5a5242a0b11a6af7f93acd
Maddie02 <madlensarkisian@gmail.com>
2023-02-19 21:36:44 +0200

    Add body validation to user routes

--- users/users.routes.config.ts
+++ users/users.routes.config.ts
@@ -2,6 +2,8 @@ import { CommonRoutesConfig } from '../common/common.routes.config'
 import express from 'express'
 import usersController from './controllers/users.controller'
 import usersMiddleware from './middleware/users.middleware'
+import { body } from 'express-validator'
+import bodyValidationMiddleware from '../common/middleware/body.validation.middleware'
 
 export class UserRoutes extends CommonRoutesConfig {
     constructor(app: express.Application) {
@@ -13,7 +15,11 @@ export class UserRoutes extends CommonRoutesConfig {
             .route('/users')
             .get(usersController.listUsers)
             .post(
-                usersMiddleware.validateRequiredUserBodyFields,
+                body('email').isEmail(),
+                body('password')
+                    .isLength({ min: 5 })
+                    .withMessage('Must include password (5+ characters)'),
+                bodyValidationMiddleware.verifyBodyFieldsErrors,
                 usersMiddleware.validateSameEmailDoesntExist,
                 usersController.createUser
             )
@@ -27,12 +33,28 @@ export class UserRoutes extends CommonRoutesConfig {
             .delete(usersController.removeUser)
 
         this.app.put('/users/:userId', [
-            usersMiddleware.validateRequiredUserBodyFields,
+            body('email').isEmail(),
+            body('password')
+                .isLength({ min: 5 })
+                .withMessage('Must include password (5+ characters)'),
+            body('firstName').isString(),
+            body('lastName').isString(),
+            body('permissionFlags').isInt(),
+            bodyValidationMiddleware.verifyBodyFieldsErrors,
             usersMiddleware.validateSameEmailBelongToSameUser,
             usersController.put,
         ])
 
         this.app.patch('/users/:userId', [
+            body('email').isEmail(),
+            body('password')
+                .isLength({ min: 5 })
+                .withMessage('Must include password (5+ characters)')
+                .optional(),
+            body('firstName').isString().optional(),
+            body('lastName').isString().optional(),
+            body('permissionFlags').isInt().optional(),
+            bodyValidationMiddleware.verifyBodyFieldsErrors,
             usersMiddleware.validatePatchEmail,
             usersController.patch,
         ])
