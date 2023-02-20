af235c08ab7f83174c7cb2556a19b61a59148241
Maddie02 <madlensarkisian@gmail.com>
2023-02-20 13:30:29 +0200

    Added logic for user permissions

--- users/middleware/users.middleware.ts
+++ users/middleware/users.middleware.ts
@@ -23,8 +23,7 @@ class UsersMiddleware {
         res: express.Response,
         next: express.NextFunction
     ) {
-        const user = await userService.getUserByEmail(req.body.email)
-        if (user && user._id === req.params.userId) {
+        if (res.locals.user._id === req.params.uderId) {
             next()
         } else {
             res.status(400).send({ error: `Invalid email` })
@@ -52,6 +51,7 @@ class UsersMiddleware {
     ) {
         const user = await userService.readById(req.params.userId)
         if (user) {
+            res.locals.user = user
             next()
         } else {
             res.status(404).send({
@@ -67,6 +67,23 @@ class UsersMiddleware {
     ) {
         req.body.id = req.params.userId
         next()
+    }
+
+    async userCantChangePermission(
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+    ) {
+        if (
+            'permissionFlags' in req.body &&
+            req.body.permissionFlags !== res.locals.user.permissionFlags
+        ) {
+            res.status(400).send({
+                errors: ['User cannot change permission flags'],
+            })
+        } else {
+            next()
+        }
     }
 }
 
